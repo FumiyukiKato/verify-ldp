5c7e5f933467e4807c8ac3cb8429aec6a318c448
FumiyukiKato <fumilemon79@gmail.com>
2020-01-17 00:19:51 +0900

    remove set.sh

--- set.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/zsh
-
-export RA_SPID=EBF7AE35A2AB286FE81D853BC0968B25
-
-SCRIPT_DIR=$(\cd $(dirname $0); pwd)
-export SERVER_KEY="${SCRIPT_DIR}/server.key"
-export SERVER_CRT="${SCRIPT_DIR}/server.crt"
-export CA="${SCRIPT_DIR}/Intel_SGX_Attestation_RootCA.pem"
